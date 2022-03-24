739c1ad2d2f8bc897af4150cfdae5b7449c4e562
xiaotong-tong <wtt150249@gmail.com>
2022-03-24 22:16:09 +0800

    chang nuxt to vue3

--- browser/middleware/mobile.js
+++
@@ -1,6 +0,0 @@
-export default function ({ route, redirect, req }) {
-    const ua = (process.server ? req.headers['user-agent']: navigator.userAgent).match(/(Android|webOS|iPhone|iPod|tablet|BlackBerry|Mobile)/i);
-    if (ua) {
-        // return redirect('/m' + route.fullPath);
-    }
-  }
