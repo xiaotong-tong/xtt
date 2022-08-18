100fb92ce5cc550d628a130fc44bede906726957
xiaotong-tong <wtt150249@gmail.com>
2022-08-18 20:58:39 +0800

    升级了showtext

--- common/showtext/src/BrowserReplaceText.js
+++ common/showtext/src/BrowserReplaceText.js
@@ -2,9 +2,14 @@ import ReplaceText from "./ReplaceText.js";
 
 class BrowserReplaceText extends ReplaceText {
 	static reverseText(text) {
-		let resText = '';
+		let resText = "";
 		// 因为命令文本反转后无法正常解析，此处将一些命令用 {{{}}}包围，然后反转文本时将 {{{}}}内的内容文本排除
-		text.split(/({{{[\s\S]*?}}})/).forEach(text => resText += /{{{[\s\S]*}}}/.test(text) ? text : super.reverseText(text));
+		text.split(/({{{[\s\S]*?}}})/).forEach(
+			(text) =>
+				(resText += /{{{[\s\S]*}}}/.test(text)
+					? text
+					: super.reverseText(text))
+		);
 		return resText;
 	}
 	static getRubyHTML(text, rt) {
@@ -13,16 +18,19 @@ class BrowserReplaceText extends ReplaceText {
 	static setTextColor(text, color) {
 		// 如果文本就是一个标签文本的话 那直接追加 style属性，不是的话那就追加一个span标签
 		if (/^<[\s\S]+>$/.test(text)) {
-			return text.replace(/^<([\s\S]+?)>/, `<$1 style="color: ${color}">`);
+			return text.replace(
+				/^<([\s\S]+?)>/,
+				`<$1 style="color: ${color}">`
+			);
 		} else {
 			return `<span style="color: ${color}" >${text}</span>`;
 		}
 	}
 	static getHeimuHTML(text) {
 		if (/^<[\s\S]+>$/.test(text)) {
-			return text.includes("class=") ?
-				text.replace(/class="([\s\S]+?)"/, "class='$1 heimu'") :
-				text.replace(/^<([\s\S]+?)>/, "<$1 class='heimu'>");
+			return text.includes("class=")
+				? text.replace(/class="([\s\S]+?)"/, "class='$1 heimu'")
+				: text.replace(/^<([\s\S]+?)>/, "<$1 class='heimu'>");
 		} else {
 			return `<span class="heimu">${text}</span>`;
 		}
