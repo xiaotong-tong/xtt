f7b5a3a1e68af15dc4ecbaebca8fb8525bd49706
小恸恸 <wtt150249@gmail.com>
2023-04-15 21:17:40 +0800

    删除不需要的文件

--- src/router/index.js
+++ src/router/index.js
@@ -2,36 +2,28 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/home.vue";
 import Text from "../pages/text.vue";
 import Lian from "../pages/lian.vue";
-import LoveWithYou from "../pages/lovewithyou/index.vue";
 
 const routes = [
 	{
 		path: "/",
 		name: "home",
 		component: Home
-	}, {
+	},
+	{
 		path: "/text",
 		name: "text",
 		component: Text
-	}, {
+	},
+	{
 		path: "/lian",
 		name: "lian",
 		component: Lian
 	}
 ];
-routes.push(...[
-	{
-		path: "/lovewithyou/",
-		name: "lovewithyou",
-		component: LoveWithYou
-	}
-]);
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes
 });
-
-// navigator.userAgent.match(/(Android|webOS|iPhone|iPod|tablet|BlackBerry|Mobile)/i);
 
 export default router;
